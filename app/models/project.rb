8dad27a4f806db1a998b5cdffd207c88dd409755
Karol Sarnacki <karolsarnacki@users.noreply.github.com>
2015-04-25 17:26:37 +0200

    Add future date validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,18 @@
 class Project < ActiveRecord::Base
 
-  validates :title, presence: true
+  validates :title, :description, :goal, :deadline,
+            presence: true
 
-  validates :goal, numericality: {greater_than: 50}
+  validates :goal,
+            numericality: {greater_than: 50}
+
+  validate :deadline_is_in_the_future
+
+  private
+
+  def deadline_is_in_the_future
+    errors.add(:deadline, 'must be in the future') if deadline && deadline < Time.current
+  end
+
 
 end
